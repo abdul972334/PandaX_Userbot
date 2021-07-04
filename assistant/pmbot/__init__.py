9de12fe8ffcd14b96ba56d8e860f7b2242a8acb2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-04 18:28:22 +0700

    Update __init__.py

--- assistant/pmbot/__init__.py
+++ assistant/pmbot/__init__.py
@@ -4,4 +4,4 @@ from PandaX_Userbot.functions.botchat_db import *
 from .. import *
 
 OWNER_NAME = petercordpanda_bot.me.first_name
-OWNER_ID = petercordpanda_bot_bot.me.id
+OWNER_ID = petercordpanda_bot.me.id
