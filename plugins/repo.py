7cdc0ac52fe470f7b1900210a08003cddff5f46e
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-04 17:50:46 +0700

    Create repo.py

---
+++ plugins/repo.py
@@ -0,0 +1,24 @@
+
+
+from telethon.errors import ChatSendInlineForbiddenError
+from telethon.errors.rpcerrorlist import BotMethodInvalidError as bmi
+
+from . import *
+
+REPOMSG = (
+    "💐 **Panda USERBOT** 💐\n\n",
+    "💐•💐 REPO - [REPO](https://github.com/IlhamMansiez/PetercordPanda)\n",
+    "💐•💐 ADDONS - [ADDONS](https://github.com/IlhamMansiez/PetercordPandaAddons)\n",
+    "💐•💐 SUPPORT - @TEAMSquadUserbotSupport",
+)
+
+
+@ilhammansiz_cmd(pattern="repo$")
+async def repify(e):
+    try:
+        q = await petercordpanda_bot.inline_query(asst.me.username, "repo")
+        await q[0].click(e.chat_id)
+        if e.sender_id == petercordpanda_bot.uid:
+            await e.delete()
+    except (ChatSendInlineForbiddenError, bmi):
+        await eor(e, REPOMSG)
